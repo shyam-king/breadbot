0270ebbe30f5a7e386148480afcd7e7caafa889e
Shyam M <m.shyam.tnj@gmail.com>
2019-03-12 19:05:33 +0530

    Troubleshoot IR com --fail

--- IRReceive/IRReceive.ino
+++ IRReceive/IRReceive.ino
@@ -1,62 +1,62 @@
 #include<avr/io.h>
 #include<avr/interrupt.h>
 
-volatile byte readingStart = 0;
-volatile byte data = 0;
+byte readingStart = 0;
+volatile uint32_t data = 0;
 volatile int nbits = 0;
+volatile int debug = 0, debug2 = 0, debug3 = 0;
+volatile float debug4 = 0;
 
 void ReceiverInit()
 {
   TCCR1A=0;
   TCNT1 = 0;
-  EICRA=(1<<ISC01);                               // enable falling edge interrupt.
+  TCCR1B = (1 << CS10) | (1 << CS11); //64 prescaler
+  TCCR1A = (1 << WGM
+  SREG |= (1 << 7); //enable interrupts
+  EICRA=(1<<ISC01);   // enable falling edge interrupt.
   EIMSK=(1<<INT0); 
 }
 
 
 ISR(INT0_vect)
 {
+  //debug = readingStart;
   if (!readingStart && !nbits)
   {
     TCNT1 = 0;
-    TCCR1B = (1 << CS10) | (1 << CS11);
-    readingStart = 1;  
+    readingStart = 1;
   }
-  if (readingStart == 1)
+  else if (readingStart == 1)
   {
-     if (TCNT1 >= 250*4 - 10 && TCNT1 <= 1010)  
+     if (TCNT1 >= 250*13 && TCNT1 <= 250*15)  
      {
        readingStart = 2;
+       //TCCR1B = (1 << CS10) ; // no prescaler
        TCNT1 = 0;
        data = 0;
-       nbits = 0;
-     }
-     else
-     {
-       readingStart = 0;
-       TCNT1 = 0;
-       TCCR1B = 0;
-       data = 0;
-       nbits = 0;
      }
    }
-   if (readingStart == 2)
+   else if (readingStart == 2)
    {
+     int t = TCNT1/250;
+     //debug3 = t;
+     TCNT1 = 0;
+     //debug2++;
        //reading the data
-       if (TCNT1 >= 250 *2 - 10 && TCNT1 <= 510)
+       if (t >= 2) // reading 1
        {
-         data |= (1 << nbits);
-         nbits ++;
-       }      
-       else if (TCNT1 < 490 || TCNT1 > 760)
+         data |= ((uint32_t)1 << (uint32_t)(31-nbits));
+       }  
+       else  // reading 0
        {
-         readingStart = 0;
-         TCNT1 = 0;
-         TCCR1B = 0;
-         data = 0;
-         nbits = 0;
-       }
-       
+        // do nothing
+       } 
+       nbits ++;
+       if (nbits == 32)
+       {
+          readingStart = 0; 
+       }     
    }
 }
 
@@ -65,13 +65,21 @@ int main()
 {
   DDRD &= !(1 << 2); //ir receiver pd2
   ReceiverInit();
-  sei();
   Serial.begin(9600);
   while (1)
   {
-    if (nbits == 8)
+    /*Serial.print("Debug: ");
+    Serial.print(debug);
+    Serial.print(" ");
+    Serial.print(debug2);
+    Serial.print(" ");
+    Serial.print(debug3);
+    Serial.print(" ");
+    Serial.println(debug4);*/
+    if (nbits == 32)
     {
-      Serial.println(data);
+      Serial.print("Data: ");
+      Serial.println(data, HEX);
       nbits = 0; 
     }
   }  
--- IRemit/IRemit.ino
+++ IRemit/IRemit.ino
@@ -1,108 +1,158 @@
 #include<avr/io.h>
 #include<avr/interrupt.h>
 
+#define BIT_START 0
+#define BIT_1 1
+#define BIT_0 2
+#define BIT_BURST 3
+#define timer1 ((1 << WGM12) | (1 << CS11) | (1 << CS10))
+
 //IR LEDs at PD2
 
-int msticks = 250;
+volatile int debug= 0;
+int LED = (1 << 2);
 
-void EmitterInit()
+void Init()
 {
-  TCCR1A=(1<<WGM01);
-  TIMSK1=(1<<OCIE0A);                    //Timer0 Interrupt on Compare Match A                       //enable global interrupts
-  DDRD=0b00000100;                       //LED Pin output
-  PORTD=0x00;
+  //global interrupts
+  SREG |= (1 << 7);
+  
+  //timers
+  TCCR0A=(1<<WGM01); //timer0 CTC
+  OCR0A = 210; // 38Khz transmission
+  TIMSK0=(1<<OCIE0A);//Timer0 Interrupt on Compare Match A      
+  
+  TCCR1B |= (1 << WGM12); //timer 1 CTC
+  TIMSK1 = (1<<OCIE1A); //timer 1 interrupt
+  
+  TCCR2A = (1 << WGM21); //timer2 CTC
+  TIMSK2 = (1 << OCIE2A); //timer 2 interrupt
+
+  //IR LED  
+  DDRD |= LED; //set output
+  PORTD &= !LED; //turn it off
+  
+  Serial.begin(9600);
 }
 
-
-void sendData(byte b)
+void IR_emit_ms(float ms)
 {
+  TCCR0B = (1 << CS00); //no prescaler
+  TCNT0 = 0;
+  TCCR1B = timer1; // 64 prescalar and ctc
+  OCR1A = 250 * ms; //250 ticks of 64 prescaler is 1 ms
+  TCNT1 = 0;
+  PORTD |= LED; //turn IR on
+}
 
-  //start bit
-      TCNT1 = 0;
-      OCR1A = msticks*3; //3ms
-      PORTD ^= 4;
-      TCCR1B = (1 << CS10) | (1 << CS11);
-      while(TCCR1B & ((1 << CS10) | (1 << CS11)));
-      TCNT1 = 0;
-      OCR1A = msticks; //1ms
-      PORTD ^= 4;
-      TCCR1B = (1 << CS10) | (1 << CS11);
-      while(TCCR1B & ((1 << CS10) | (1 << CS11)));
-  
-  
+void IR_emit_us(float us)
+{
+  TCCR0B = (1 << CS00); //no prescaler
+  TCNT0 = 0;
+  TCCR1B = (1 << CS00); // no prescalar
+  OCR1A = 16 * us; //250 ticks of 64 prescaler is 1 ms
+  TCNT1 = 0;
+  PORTD |= LED; //turn IR on
+}
 
-  for (int i = 0 ; i < 8;i++){
-    if (b & (1 << i)){
-      
-      //send 1
-      TCNT1 = 0;
-      OCR1A = msticks; //1ms
-      PORTD ^= 4;
-      TCCR1B = (1 << CS10) | (1 << CS11);
-      while(TCCR1B & ((1 << CS10) | (1 << CS11)));
-      TCNT1 = 0;
-      OCR1A = msticks; //1ms
-      PORTD ^= 4;
-      TCCR1B = (1 << CS10) | (1 << CS11);
-      while(TCCR1B & ((1 << CS10) | (1 << CS11)));
+void IR_stop()
+{
+  TCCR0B = 0;//stop timer 0
+  PORTD &= !LED; //turn off IR
+}
+
+ISR(TIMER0_COMPA_vect)
+{
+  PORTD ^= LED; //toggle (50% duty cycle) 
+}
+
+ISR(TIMER1_COMPA_vect)
+{
+  TCCR1B = 0;//stop timer 1
+  IR_stop();//stop emitting
+}
+
+ISR(TIMER2_COMPA_vect)
+{
+   TCCR2B = 0; 
+}
+
+byte IR_emitting()
+{  
+  return TCCR1B;  
+}
+
+void wait_ms(float ms)
+{
+  TCCR2B = (1 << CS21) | (1<<CS20);
+  OCR2A = 250 * ms;
+  TCNT2 = 0;
+  while (TCCR2B);
+}
+
+void wait_us(float us)
+{
+  TCCR2B = (1 << CS20);
+  OCR2A = 16 * us;
+  TCNT2 = 0;
+  while (TCCR2B);
+}
+
+void sendPulse(byte Bit)
+{
+  switch(Bit)
+  {
+     case BIT_START: 
+       IR_emit_ms(9); //9ms HIGH
+       while(IR_emitting()); //wait
+       wait_ms(4); //1ms LOW
+     break;
      
-    } else {
-      
-      //send 0
-      TCNT1 = 0;
-      OCR1A = msticks; //1ms
-      PORTD ^= 4;
-      TCCR1B = (1 << CS10) | (1 << CS11);
-      while(TCCR1B & ((1 << CS10) | (1 << CS11)));
-      TCNT1 = 0;
-      OCR1A = msticks*2; //2ms
-      PORTD ^= 4;
-      TCCR1B = (1 << CS10) | (1 << CS11);
-      while(TCCR1B & ((1 << CS10) | (1 << CS11)));
-      
-    }
-  }
-  //complement
-  for (int i = 0 ; i < 8;i++){
-    if (!(b & (1 << i))){
-      
-      //send 1
-      TCNT1 = 0;
-      OCR1A = msticks; //1ms
-      PORTD ^= 4;
-      TCCR1B = (1 << CS10) | (1 << CS11);
-      while(TCCR1B & ((1 << CS10) | (1 << CS11)));
-      TCNT1 = 0;
-      OCR1A = msticks; //1ms
-      PORTD ^= 4;
-      TCCR1B = (1 << CS10) | (1 << CS11);
-      while(TCCR1B & ((1 << CS10) | (1 << CS11)));
+     case BIT_1: 
+       IR_emit_ms(1); //2us HIGH
+       while(IR_emitting()); //wait
+       wait_ms(2.5); //1us LOW
+     break;
      
-    } else {
-      
-      //send 0
-      TCNT1 = 0;
-      OCR1A = msticks; //1ms
-      PORTD ^= 4;
-      TCCR1B = (1 << CS10) | (1 << CS11);
-      while(TCCR1B & ((1 << CS10) | (1 << CS11)));
-      TCNT1 = 0;
-      OCR1A = msticks*2; //2ms
-      PORTD ^= 4;
-      TCCR1B = (1 << CS10) | (1 << CS11);
-      while(TCCR1B & ((1 << CS10) | (1 << CS11)));
-      
-    }
+     case BIT_0: 
+       IR_emit_ms(1); //1us HIGH
+       while(IR_emitting()); //wait
+       wait_ms(1.25); //1us LOW
+     break;
+     
+     case BIT_BURST:
+       IR_emit_ms(20);
+       while(IR_emitting()); //wait
   }
+}
+
+void sendData(uint32_t d)
+{
+  //sendPulse(BIT_BURST);
+  
+  for(int i = 0; i < 32; i++)
+  {
+    if (d & 1) // right most bit
+    {
+      sendPulse(BIT_1);
+    }
+    else
+    {
+      sendPulse(BIT_0); 
+    }
+    d >>= 1;
+  } 
 }
 
 int main()
 {
-	EmitterInit();
-	Serial.begin(9600);
-	//TCCR1B = (1 << CS00) | (1 << CS01);
-	sei();
-
-        while(1) {sendData(65);delay(100);}
-        return 0;
+  Init();
+  
+  while(1) {
+    sendPulse(BIT_START);
+    sendData(0);
+    sendPulse(BIT_BURST);
+    wait_ms(100);
+  }
+  return 0;
 }
